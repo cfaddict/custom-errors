469caa517a9c7b75d626b79b0eeccdefca5ede31
Dan Vega <danvega@gmail.com>
2017-03-23 18:32:00 -0400

    Final Update of code for article on custom errors

--- src/main/java/com/therealdanvega/controller/HomeController.java
+++ src/main/java/com/therealdanvega/controller/HomeController.java
@@ -1,9 +1,8 @@
 package com.therealdanvega.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
 
 
